85eb035cb260e616e70abcaa4195e1336311a62a
Joshua Yoon <joshyoon92@gmail.com>
2021-04-14 10:50:13 +0900

    Initial commit

---
+++ src/com/company/SumLists.java
@@ -0,0 +1,24 @@
+package com.company;
+import java.util.LinkedList;
+
+public class SumLists {
+    public LinkedList sumLists(LinkedList l1, LinkedList l2, int carry){
+        if (l1 == null && l2 ==null && carry ==0){
+            return null;
+        }
+        LinkedList<Integer> result = new LinkedList<>();
+        int value = carry;
+        if (l1 != null) {
+            value += l1.data;
+        }
+        if (l2 != null) {
+            value +=l1.data;
+        }
+        result.data = value%10;
+        if (l1 != null || l2 != null) {
+            LinkedList more = addLists(l1 ==null ? null : l1.next(), l2 == null ? null : l2.next, value >= 10 ? 1:0);
+            result.setNext(more);
+        }
+        return result;
+    }
+}
